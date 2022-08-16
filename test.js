a63693a8b36b9df84e9b68d62b8be1abdf3670f0
Leo Ma <forza.leoma@gmail.com>
2022-08-16 10:56:23 +0200

    feat: add a new line

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 console.log('no fat princess');
-
 console.log('this is a new feature');
 
 console.log('add a very good c ommit');
+console.log('add a very good c ommit');
+console.log('add a very good c ommit');
+console.log('add a very good c ommit');
 
+console.log('add a very good c ommit');s
